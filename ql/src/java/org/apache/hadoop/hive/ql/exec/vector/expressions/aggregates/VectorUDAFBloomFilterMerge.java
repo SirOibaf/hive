f9550482a4f309145a470edef9b3b1629db0dc1a
Prasanth Jayachandran <prasanthj@apache.org>
2017-08-04 11:47:08 -0700

    HIVE-17220: Bloomfilter probing in semijoin reduction is thrashing L1 dcache (Prasanth Jayachandran reviewed by Gopal V)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/aggregates/VectorUDAFBloomFilterMerge.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/aggregates/VectorUDAFBloomFilterMerge.java
@@ -26,17 +26,16 @@ import org.apache.hadoop.hive.ql.exec.vector.ColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.VectorAggregationBufferRow;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.hadoop.hive.ql.exec.vector.expressions.VectorExpression;
-import org.apache.hadoop.hive.ql.exec.vector.expressions.aggregates.VectorAggregateExpression.AggregationBuffer;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.plan.AggregationDesc;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFBloomFilter.GenericUDAFBloomFilterEvaluator;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.apache.hadoop.io.BytesWritable;
-import org.apache.hive.common.util.BloomFilter;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hive.common.util.BloomKFilter;
 
 public class VectorUDAFBloomFilterMerge extends VectorAggregateExpression {
-
   private static final long serialVersionUID = 1L;
 
   private VectorExpression inputExpression;
@@ -59,13 +58,16 @@ public class VectorUDAFBloomFilterMerge extends VectorAggregateExpression {
     byte[] bfBytes;
 
     public Aggregation(long expectedEntries) {
+      ByteArrayOutputStream bytesOut = null;
       try {
-        BloomFilter bf = new BloomFilter(expectedEntries);
-        ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
-        BloomFilter.serialize(bytesOut, bf);
+        BloomKFilter bf = new BloomKFilter(expectedEntries);
+        bytesOut = new ByteArrayOutputStream();
+        BloomKFilter.serialize(bytesOut, bf);
         bfBytes = bytesOut.toByteArray();
       } catch (Exception err) {
         throw new IllegalArgumentException("Error creating aggregation buffer", err);
+      } finally {
+        IOUtils.closeStream(bytesOut);
       }
     }
 
@@ -77,7 +79,7 @@ public class VectorUDAFBloomFilterMerge extends VectorAggregateExpression {
     @Override
     public void reset() {
       // Do not change the initial bytes which contain NumHashFunctions/NumBits!
-      Arrays.fill(bfBytes, BloomFilter.START_OF_SERIALIZED_LONGS, bfBytes.length, (byte) 0);
+      Arrays.fill(bfBytes, BloomKFilter.START_OF_SERIALIZED_LONGS, bfBytes.length, (byte) 0);
     }
   }
 
@@ -365,7 +367,7 @@ public class VectorUDAFBloomFilterMerge extends VectorAggregateExpression {
     // BloomFilter.mergeBloomFilterBytes() does a simple byte ORing
     // which should be faster than deserialize/merge.
     BytesColumnVector inputColumn = (BytesColumnVector) columnVector;
-    BloomFilter.mergeBloomFilterBytes(myagg.bfBytes, 0, myagg.bfBytes.length,
+    BloomKFilter.mergeBloomFilterBytes(myagg.bfBytes, 0, myagg.bfBytes.length,
         inputColumn.vector[i], inputColumn.start[i], inputColumn.length[i]);
   }
 }
